ab479cdbfae3e5005035de2e5a739ca8a64cdacd
kalindev <nescauzitinho@gmail.com>
2022-11-16 18:02:39 -0300

    fix: uning useEffect to fix error

--- src/components/radioGroup/RadioGroup.tsx
+++ src/components/radioGroup/RadioGroup.tsx
@@ -2,45 +2,45 @@ import * as RadioGroup from '@radix-ui/react-radio-group';
 import './styles.css';
 import { LabelForm } from '../Label';
 import { InputText } from "../InputText"
-import { useState, useEffect, FormEvent, ChangeEvent, InputHTMLAttributes } from 'react';
+import { useState, useEffect, InputHTMLAttributes } from 'react';
 
 interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
     values: string[];
     label: string;
     showDescription?: boolean;
-    onAnswer: Function
+    onAnswer: Function;
+    questionId: string;
 }
 
 export function RadioGroupDemo(props: RadioProps) {
-    const random = Math.random()
 
-    const [radioValue, setRadioValue] = useState<string>();
-    const [description, setdescription] = useState<string>();
+    const [radioValue, setRadioValue] = useState<string>("");
+    const [description, setdescription] = useState<string>("");
 
-    useEffect(() => sendAnswer, [radioValue, description]);
+    useEffect(() => sendAnswer(radioValue, description), [radioValue, description]);
 
-    function sendAnswer(){
+    function sendAnswer(radioValue:string, description:string){
         props.onAnswer(
         {
-            pergunta: props.label,
-            valor: radioValue || null ,
-            descricao: description || null
+            value: radioValue || null ,
+            description: description || null,
+            questionId: props.questionId
         })
     }
 
     let radios = props.values.map((item, index) => {
         return (
 
-            <div className='flex items-center gap gap-4' key={Math.random()}>
+            <div className='flex items-center gap gap-4' key={index + props.questionId}>
                 <RadioGroup.Item
                     className="bg-white w-6 h-6 rounded-xl shadow-md shadow-slate-800 hover:bg-violet-200"
                     value={String(index)}
-                    id={random + String(index)}
+                    id={String(index)}
                     onClick={e => setRadioValue(e.currentTarget.value)}
                 >
                     <RadioGroup.Indicator className="RadioGroupIndicator" />
                 </RadioGroup.Item>
-                <LabelForm label={item} htmlFor={random + String(index)} />
+                <LabelForm label={item} htmlFor={ String(index)} />
             </div>
         )
     })
