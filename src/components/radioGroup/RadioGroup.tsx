3df96b03bb7b4b5d6fdd2a1d57ccbb6ea324d378
kalindev <nescauzitinho@gmail.com>
2022-11-19 11:45:03 -0300

    dennis

--- src/components/radioGroup/RadioGroup.tsx
+++ src/components/radioGroup/RadioGroup.tsx
@@ -5,7 +5,7 @@ import { InputText } from "../InputText"
 import { useState, useEffect, InputHTMLAttributes, useRef } from 'react';
 
 interface RadioProps extends InputHTMLAttributes<HTMLInputElement> {
-    values: string[];
+    values: any[];
     label: string;
     showDescription?: boolean;
     onAnswer: Function;
@@ -16,18 +16,19 @@ export function RadioGroupDemo(props: RadioProps) {
 
     const [radioValue, setRadioValue] = useState<string>("");
     const [description, setdescription] = useState<string>("");
- 
-    let firstTime = useRef(true)
-    useEffect(() => { firstTime.current ? firstTime.current = false : sendAnswer(radioValue, description)}, [radioValue, description]);
 
-    function sendAnswer(radioValue:string, description:string){
+    useEffect(() => { sendAnswer(radioValue, description) }, [radioValue, description]);
+
+    function sendAnswer(radioValue: string, description: string) {
         props.onAnswer(
-        {
-            value: radioValue || null ,
-            description: description || null,
-            questionId: props.questionId
-        })
+            {
+                value: radioValue || null,
+                description: description || null,
+                questionId: props.questionId
+            })
     }
+
+
 
     let radios = props.values.map((item, index) => {
         return (
@@ -35,29 +36,37 @@ export function RadioGroupDemo(props: RadioProps) {
             <div className='flex items-center gap gap-4' key={index + props.questionId}>
                 <RadioGroup.Item
                     className="bg-white w-6 h-6 rounded-xl shadow-md shadow-slate-800 hover:bg-violet-200"
-                    value={String(index)}
-                    id={String(index)}
-                    onClick={e => setRadioValue(e.currentTarget.value)}
+                    value={item.value}
+                    onClick={e => {
+                        if (e.currentTarget.value == "0") {
+                            setdescription("")
+                        }
+                        setRadioValue(e.currentTarget.value)
+
+                    }}
                 >
                     <RadioGroup.Indicator className="RadioGroupIndicator" />
                 </RadioGroup.Item>
-                <LabelForm label={item} htmlFor={ String(index)} />
+                <LabelForm label={item.label} htmlFor={index + props.questionId} />
             </div>
         )
     })
 
     return (
-        <div className='mt-10 bg-white p-4 rounded-md border-2'>
-            <strong><LabelForm label={props.label} /></strong>
-            <RadioGroup.Root className="flex flex-col gap gap-2.5" defaultValue="default" aria-label="View density">
-                <div className='flex flex-col gap gap-2 '>
-                    {radios}
-                </div>
-            </RadioGroup.Root>
+        <>
 
-            {props.showDescription && Number(radioValue) > 0 
-            ? <InputText placeholder='Descreva por favor' value={description} onChange={(e) => setdescription(e.target.value)} /> 
-            : ""}
-        </div>
+            <div className='mt-10 bg-white p-4 rounded-md border-2'>
+                <strong><LabelForm label={props.label} /></strong>
+                <RadioGroup.Root className="flex flex-col gap gap-2.5" defaultValue="default" aria-label="View density">
+                    <div className='flex flex-col gap gap-2 '>
+                        {radios}
+                    </div>
+                </RadioGroup.Root>
+
+                {props.showDescription && Number(radioValue) > 0
+                    ? <InputText placeholder='Descreva por favor' value={description} onChange={(e) => setdescription(e.target.value)} />
+                    : ""}
+            </div>
+        </>
     )
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from 'react';
-import { HealthSection } from "../components/sections/HealthSection";
-import { SymptomsSection } from "../components/sections/SymptomsSection";
+import { useEffect, useState } from 'react';
+import { InputText } from '../components/InputText';
+import { RadioGroupDemo } from '../components/radioGroup/RadioGroup';
+import { useFetch } from '../hooks/useFetch';
+
 
 interface Answer {
   questionId: string;
@@ -8,47 +10,81 @@ interface Answer {
   description: string
 }
 
+
+interface section {
+  name: string,
+  questions: any
+}
+
+
 export function Home() {
 
-  const [section, setSection] = useState<number>(0);
-  useEffect(() => attSection(section), [section]);
-
-  let answers: Answer[] = [];
+  const { data: sections, isFetching } = useFetch<section[]>('/section')
+  const [currentSection, setCurrentSection] = useState<number>(0);
+  const [answers, setAnswers] = useState<Answer[]>([]);
   
+
+  useEffect(() => { generateQuestions() }, [answers]);
+
+
+  let skipSection = false
+  function generateQuestions() {
+
+    if (sections == null) return;
+    
+    return sections[currentSection].questions.filter((question: any)  => {
+      
+      if(skipSection) return false;
+
+      if(question.radios.length == 0) return true;
+
+      let radioWithAction = question.radios.find((radio: any) => radio.action == 0)
+
+      if (radioWithAction && (answers.findIndex(ans => ans.questionId == question.id && (ans.value == null ||  ans.value == radioWithAction.value))) ) {
+        skipSection = true
+        return true;}
+      else if (radioWithAction){
+        skipSection = false
+        return true
+      }
+    console.log(skipSection)
+
+    }).map((item: any) => {
+      switch (item.type) {
+        case 0:
+          return <InputText label={item.label} />
+        case 1:
+          return <RadioGroupDemo label={item.label} values={item.radios} questionId={item.id} onAnswer={(answer: Answer) => handleAnswers(answer)} />
+        case 3:
+          return  <RadioGroupDemo label={item.label} values={item.radios} questionId={item.id} onAnswer={(answer: Answer) => handleAnswers(answer)} showDescription={true}/>
+      }
+    })
+  }
+
+
+
   function handleAnswers(answer: Answer) {
     const indexAnswer = answers.findIndex(ans => ans.questionId === answer.questionId);
     if (indexAnswer > -1) answers[indexAnswer] = answer;
-    else answers.push(answer);
-    console.log(answer)
+    else  answers.push(answer);
+    
+    setAnswers(answers)
+    console.log(answers)
   }
-
-  function attSection(sectionValue: number) {
-    handleCurrentSection(sectionValue)
-  }
-
-  function handleCurrentSection(sectionValue: number) {
-
-    if (sectionValue < 0) sectionValue = 0
-
-    //troca isso para um switch karai (*-*)
-    if (sectionValue === 0) return <HealthSection onAnswer={(answer: Answer) => handleAnswers(answer)} />
-    else if (sectionValue === 1) return <SymptomsSection onAnswer={(answer: Answer) => handleAnswers(answer)} />
-  }
-
 
 
 
   return (
     <>
 
-      {
-        handleCurrentSection(section)
-      }
-
-      <button onClick={() => setSection(section - 1)}>VOLTAR</button>
-      <br />
-      <button onClick={() => setSection(section + 1)}>PRÓXIMA</button>
-
+      <div className="flex flex-col justify-center items-center bg-slate-50">
+        <form className="min-h-screen max-w-5xl mt-5 mb-5">
+          <h1 className='text-bluePurple-500 text-2xl font-bold'>{sections? sections[0].name: ""}</h1>
+          <hr />
+          {isFetching && <p>Carregando...</p>}
+          {generateQuestions()}
+        </form>
+      </div>
     </>
   )
 }
