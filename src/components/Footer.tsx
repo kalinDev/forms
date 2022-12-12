4deb857cc687de0de4089f063e6f64558fefae41
kalindev <nescauzitinho@gmail.com>
2022-12-12 18:54:48 -0300

    feature: add meta2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
+
 export function Footer() {
+  const navigateFooter = useNavigate();
   return (
     <footer className="bg-slate-100  h-16 flex justify-center items-center relative bottom-0 w-full ">
-      <span className="text-black font-bold text-md">@2022 Copyrigth</span>
+      <span onClick={() => navigateFooter("/agradecimentos")} className="text-black font-bold text-md">@2022 Copyrigth</span>
     </footer>
   );
 }
